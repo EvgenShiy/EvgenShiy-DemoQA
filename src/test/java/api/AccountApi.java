b600c6a4105a4e6c40e3096ef238abe6492ff4cb
Evgeniya Shiyanova <evgenshiy@gmail.com>
2025-01-24 02:09:10 +0500

    rename Logger

--- src/test/java/api/AccountApi.java
+++ src/test/java/api/AccountApi.java
@@ -14,11 +14,11 @@ import static specs.ApiSpecs.*;
 
 public class AccountApi {
 
-    private static final Logger logger = LoggerFactory.getLogger(AccountApi.class);
+    private static final Logger log = LoggerFactory.getLogger(AccountApi.class);
 
     @Step("Получить данные авторизации зарегистрированного пользователя через API")
     public static AuthResponseModel getAuthData(String userName, String userPassword) {
-        logger.info("Отправка запроса на авторизацию для пользователя: {}", userName);
+        log.info("Отправка запроса на авторизацию для пользователя: {}", userName);
 
         AuthRequestModel request = new AuthRequestModel();
         request.setUserName(userName);
@@ -33,13 +33,13 @@ public class AccountApi {
                 .spec(successResponse200Spec)
                 .extract().as(AuthResponseModel.class);
 
-        logger.info("Ответ на запрос авторизации получен. Токен: {}", response.getToken());
+        log.info("Ответ на запрос авторизации получен. Токен: {}", response.getToken());
         return response;
     }
 
     @Step("Генерация токена для авторизации через API")
     public static AuthResponseModel generateAuthToken(String userName, String userPassword) {
-        logger.info("Генерация токена для пользователя: {}", userName);
+        log.info("Генерация токена для пользователя: {}", userName);
 
         AuthRequestModel request = new AuthRequestModel();
         request.setUserName(userName);
@@ -54,12 +54,12 @@ public class AccountApi {
                 .spec(successResponse200Spec)
                 .extract().as(AuthResponseModel.class);
 
-        logger.info("Токен успешно сгенерирован: {}", response.getToken());
+        log.info("Токен успешно сгенерирован: {}", response.getToken());
         return response;
     }
 
     public static AuthRequestModel generateRandomUserData() {
-        logger.info("Генерация данных для нового рандомного пользователя...");
+        log.info("Генерация данных для нового рандомного пользователя...");
 
         RandomUtils randomUtils = new RandomUtils();
 
@@ -70,14 +70,14 @@ public class AccountApi {
         request.setUserName(randomUserName);
         request.setPassword(randomPassword);
 
-        logger.info("Сгенерированы данные: UserName = {}, Password = {}", randomUserName, randomPassword);
+        log.info("Сгенерированы данные: UserName = {}, Password = {}", randomUserName, randomPassword);
         return request;
     }
 
 
     @Step("Регистрация нового пользователя")
     public static AuthResponseModel registerUser(String userName, String password) {
-        logger.info("Регистрация нового пользователя с данными: UserName = {}", userName);
+        log.info("Регистрация нового пользователя с данными: UserName = {}", userName);
 
         AuthRequestModel request = new AuthRequestModel();
         request.setUserName(userName);
@@ -92,13 +92,13 @@ public class AccountApi {
                 .spec(successResponse201Spec)
                 .extract().as(AuthResponseModel.class);
 
-        logger.info("Пользователь {} успешно зарегистрирован. UserId: {}", userName, response.getUserId());
+        log.info("Пользователь {} успешно зарегистрирован. UserId: {}", userName, response.getUserId());
         return response;
     }
 
     @Step("Попытка регистрация нового пользователя с невалидным паролем")
     public static ErrorResponseModel registerUserWithError(String userName, String password) {
-        logger.info("Попытка регистрация нового пользователя с данными: UserName = {}", userName);
+        log.info("Попытка регистрация нового пользователя с данными: UserName = {}", userName);
 
         AuthRequestModel request = new AuthRequestModel();
         request.setUserName(userName);
@@ -113,13 +113,13 @@ public class AccountApi {
                 .spec(errorResponse400Spec)
                 .extract().as(ErrorResponseModel .class);
 
-        logger.info("Пользователь с данными: UserName = {} не зарегистрирован", userName);
+        log.info("Пользователь с данными: UserName = {} не зарегистрирован", userName);
         return response;
     }
 
     @Step("Получить данные профиля пользователя")
     public static UserProfileModel getUserProfile(String token, String userId) {
-        logger.info("Запрос профиля пользователя с токеном: {}", token);
+        log.info("Запрос профиля пользователя с токеном: {}", token);
 
         UserProfileModel response = given()
                 .spec(requestSpec)
@@ -130,13 +130,13 @@ public class AccountApi {
                 .spec(successResponse200Spec)
                 .extract().as(UserProfileModel.class);
 
-        logger.info("Профиль пользователя получен: {}", response);
+        log.info("Профиль пользователя получен: {}", response);
         return response;
     }
 
     @Step("Удалить пользователя")
     public static void deleteUser(String token, String userId) {
-        logger.info("Удаление пользователя с ID: {}", userId);
+        log.info("Удаление пользователя с ID: {}", userId);
 
         given()
                 .spec(requestSpec)
@@ -147,12 +147,12 @@ public class AccountApi {
                 .spec(successResponse204Spec)
                 .log().ifError();
 
-        logger.info("Пользователь с ID {} успешно удален.", userId);
+        log.info("Пользователь с ID {} успешно удален.", userId);
     }
 
     @Step("Проверить, авторизован ли пользователь через API")
     public static boolean isUserAuthorized(String token) {
-        logger.info("Проверка авторизации пользователя с токеном: {}", token);
+        log.info("Проверка авторизации пользователя с токеном: {}", token);
 
         return given()
                 .spec(requestSpec)
