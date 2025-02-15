3455d73fd5b961ce71adb55b0b59b6e91fea5fb5
Evgeniya Shiyanova <evgenshiy@gmail.com>
2025-02-15 16:10:41 +0500

    fixed Configs

--- src/test/java/tests/ui_tests/UI_TestBase.java
+++ src/test/java/tests/ui_tests/UI_TestBase.java
@@ -139,9 +139,14 @@ public class UI_TestBase {
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
 
+// Исправляем проблемы с ChromeOptions
             options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+            options.setExperimentalOption("useAutomationExtension", false);
 
             capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+            capabilities.setCapability("browserVersion", Configuration.browserVersion);
+            capabilities.setCapability("platformName", "ANY");
+
         } else {
             if (Objects.equals(Configuration.browser, "chrome")) {
                 WebDriverManager.chromedriver().setup();
