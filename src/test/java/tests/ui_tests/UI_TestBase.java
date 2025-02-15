d833807b7cd088d8c16ca52779c5c17daf29df8d
Evgeniya Shiyanova <evgenshiy@gmail.com>
2025-02-15 15:52:15 +0500

    fixed Configs

--- src/test/java/tests/ui_tests/UI_TestBase.java
+++ src/test/java/tests/ui_tests/UI_TestBase.java
@@ -1,4 +1,4 @@
-package tests.ui_tests;
+/*package tests.ui_tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
@@ -61,6 +61,94 @@ public class UI_TestBase {
         Configuration.browserCapabilities = capabilities;
     }
 
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Screenshot");
+        if (!Configuration.browser.equals("firefox")) {
+            Attach.pageSource();
+            Attach.browserConsoleLogs();
+        }
+        Attach.addVideo();
+        closeWebDriver();
+    }
+}
+
+ */
+package tests.ui_tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import config.WebDriverConfig;
+import helpers.Attach;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.selenide.AllureSelenide;
+import io.restassured.RestAssured;
+import io.restassured.parsing.Parser;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class UI_TestBase {
+    private static final WebDriverConfig config = ConfigFactory.create(WebDriverConfig.class, System.getProperties());
+
+    @BeforeAll
+    public static void setUp() {
+        RestAssured.baseURI = "https://demoqa.com";
+        RestAssured.defaultParser = Parser.JSON;
+
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = config.getPageLoadStrategy();
+        Configuration.browserSize = config.getBrowserSize();
+        Configuration.browser = config.getBrowserName().browserToLowerCase();
+        Configuration.browserVersion = config.getBrowserVersion();
+
+        String remoteUrl = config.getRemoteUrl();
+        System.out.println("DEBUG: remoteUrl from config = " + remoteUrl);
+
+        if (remoteUrl != null && !remoteUrl.isEmpty()) {
+            Configuration.remote = remoteUrl;
+        }
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        if (remoteUrl != null && !remoteUrl.isEmpty()) {
+            Map<String, Object> selenoidOptions = new HashMap<>();
+            selenoidOptions.put("enableVNC", true);
+            selenoidOptions.put("enableVideo", true);
+            capabilities.setCapability("selenoid:options", selenoidOptions);
+
+            // Добавляем ChromeOptions для headless режима
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless=new", "--no-sandbox", "--disable-dev-shm-usage");
+            capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        } else {
+            if (Objects.equals(Configuration.browser, "chrome")) {
+                WebDriverManager.chromedriver().setup();
+            } else if (Objects.equals(Configuration.browser, "firefox")) {
+                WebDriverManager.firefoxdriver().setup();
+            } else if (Objects.equals(Configuration.browser, "edge")) {
+                WebDriverManager.edgedriver().setup();
+            }
+        }
+
+        Configuration.browserCapabilities = capabilities;
+        System.out.println("DEBUG: browserCapabilities = " + capabilities);
+    }
 
     @BeforeEach
     void addListener() {
