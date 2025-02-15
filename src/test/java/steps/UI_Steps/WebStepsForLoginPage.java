ab46e51881e69a889e606b921518c4b646c1364f
Evgeniya Shiyanova <evgenshiy@gmail.com>
2025-02-15 23:27:00 +0500

    fixed LoginUITests

--- src/test/java/steps/UI_Steps/WebStepsForLoginPage.java
+++ src/test/java/steps/UI_Steps/WebStepsForLoginPage.java
@@ -1,4 +1,4 @@
-package steps.UI_Steps;
+/*package steps.UI_Steps;
 
 import config.CredentialsConfig;
 import io.qameta.allure.Step;
@@ -67,3 +67,76 @@ public class WebStepsForLoginPage {
         loginPage.verifyFieldHighlightedAsMandatory(fieldName);
     }
 }
+
+ */
+package steps.UI_Steps;
+
+import config.CredentialsConfig;
+import io.qameta.allure.Step;
+import lombok.Getter;
+import org.aeonbits.owner.ConfigFactory;
+import pages.LoginPage;
+import utils.RandomUtils;
+
+public class WebStepsForLoginPage {
+
+    private final LoginPage loginPage = new LoginPage();
+    private final RandomUtils randomUtils = new RandomUtils();
+    private static final CredentialsConfig credentials = ConfigFactory.create(CredentialsConfig.class, System.getProperties());
+
+    @Getter
+    private String lastGeneratedUserName;
+    @Getter
+    private String lastGeneratedPassword;
+
+    @Step("Открыть страницу login")
+    public void openLoginPage() {
+        loginPage.openPageRemoveBanners();
+    }
+
+    @Step("Заполнить поле UserName рандомным значением: {0}")
+    public void setRandomUserName(String randomUserName) {
+        lastGeneratedUserName = randomUserName;
+        loginPage.setUsername(lastGeneratedUserName);
+    }
+
+    @Step("Заполнить поле Password рандомным значением: {0}")
+    public void setRandomPassword(String randomPassword) {
+        lastGeneratedPassword = randomPassword;
+        loginPage.setPassword(lastGeneratedPassword);
+    }
+
+    @Step("Нажать кнопку 'Login'")
+    public void pressLoginButton() {
+        loginPage.clickLoginButton();
+    }
+
+    @Step("Авторизоваться зарегистрированным пользователем с логином {0} и паролем {1}")
+    public void verifySuccessfulLoginExistingUser() {
+        String username = credentials.getUsername();
+        String password = credentials.getPassword();
+
+        if (username == null || password == null || username.isEmpty() || password.isEmpty()) {
+            throw new IllegalStateException(
+                    "Ошибка: Логин или пароль отсутствуют. Проверь credentials.properties или передаваемые параметры в Jenkins."
+            );
+        }
+
+        openLoginPage();
+        loginPage.setUsername(username);
+        loginPage.setPassword(password);
+        pressLoginButton();
+        loginPage.verifyLogoutButtonIsVisible();
+    }
+
+    @Step("Проверить неудачную аутентификацию пользователя")
+    public void verifyErrorDisplayedForInvalidCredentials() {
+        loginPage.verifyErrorMessage("Invalid username or password!");
+    }
+
+    @Step("Проверить, что поле {0} подсвечено красной рамкой как обязательное")
+    public void verifyMandatoryFieldHighlight(String fieldName) {
+        loginPage.verifyFieldHighlightedAsMandatory(fieldName);
+    }
+}
+
--- src/test/java/tests/ui_tests/LoginUITests.java
+++ src/test/java/tests/ui_tests/LoginUITests.java
@@ -1,4 +1,4 @@
-package tests.ui_tests;
+/*package tests.ui_tests;
 
 import io.qameta.allure.Feature;
 import io.qameta.allure.Owner;
@@ -26,6 +26,54 @@ public class LoginUITests extends UI_TestBase {
     @DisplayName("Проверка неудачной аутентификации пользователя")
     void unSuccessesLoginTest() {
 
+        String randomUser = randomUtils.getRandomFirstName();
+        String randomPass = randomUtils.getRandomString(8);
+
+        webStepsForLoginPage.openLoginPage();
+        webStepsForLoginPage.setRandomUserName(randomUser);
+        webStepsForLoginPage.setRandomPassword(randomPass);
+        webStepsForLoginPage.pressLoginButton();
+        webStepsForLoginPage.verifyErrorDisplayedForInvalidCredentials();
+    }
+
+    @Test
+    @DisplayName("Проверка подсветки обязательных полей красной рамкой при отсутствии ввода")
+    public void shouldHighlightMandatoryFieldsWhenEmptyTest() {
+        webStepsForLoginPage.openLoginPage();
+        webStepsForLoginPage.pressLoginButton();
+        webStepsForLoginPage.verifyMandatoryFieldHighlight("userName");
+        webStepsForLoginPage.verifyMandatoryFieldHighlight("password");
+    }
+}
+
+ */
+package tests.ui_tests;
+
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import steps.UI_Steps.WebStepsForLoginPage;
+import utils.RandomUtils;
+
+@Tag("UI")
+@Feature("Account")
+@Owner("Shiianova E.")
+public class LoginUITests extends UI_TestBase {
+
+    private final WebStepsForLoginPage webStepsForLoginPage = new WebStepsForLoginPage();
+    private final RandomUtils randomUtils = new RandomUtils();
+
+    @Test
+    @DisplayName("Проверка успешной аутентификации зарегистрированного пользователя")
+    void successesLoginTest() {
+        webStepsForLoginPage.verifySuccessfulLoginExistingUser();
+    }
+
+    @Test
+    @DisplayName("Проверка неудачной аутентификации пользователя")
+    void unSuccessesLoginTest() {
         String randomUser = randomUtils.getRandomFirstName();
         String randomPass = randomUtils.getRandomString(8);
 
