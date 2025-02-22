d706066987e99a8a4d77c138326f2b264c9232c8
Evgeniya Shiyanova <evgenshiy@gmail.com>
2025-02-23 00:00:06 +0500

    fixed remote properties

--- src/test/java/config/WebDriverConfig.java
+++ src/test/java/config/WebDriverConfig.java
@@ -1,37 +1,3 @@
-/*package config;
-
-import org.aeonbits.owner.Config;
-
-import java.net.URL;
-
-@Config.Sources({
-        "system:properties",
-        "classpath:properties/${env}.properties",
-        "classpath:properties/local.properties"
-})
-public interface WebDriverConfig extends Config {
-
-    @Key("browserName")
-    @DefaultValue("CHROME")
-    Browser getBrowserName();
-
-    @Key("browserSize")
-    @DefaultValue("1920x1080")
-    String getBrowserSize();
-
-    @Key("browserVersion")
-    @DefaultValue("125.0")
-    String getBrowserVersion();
-
-    @Key("remoteUrl")
-    URL getRemoteUrl();
-
-    @Key("pageLoadStrategy")
-    @DefaultValue("eager")
-    String getPageLoadStrategy();
-}
-
- */
 package config;
 
 import org.aeonbits.owner.Config;
--- src/test/java/tests/ui_tests/UI_TestBase.java
+++ src/test/java/tests/ui_tests/UI_TestBase.java
@@ -1,86 +1,3 @@
-/*
-package tests.ui_tests;
-
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import config.WebDriverConfig;
-import helpers.Attach;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import io.qameta.allure.selenide.AllureSelenide;
-import io.restassured.RestAssured;
-import io.restassured.parsing.Parser;
-import org.aeonbits.owner.ConfigFactory;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
-import java.util.Map;
-import java.util.Objects;
-
-import static com.codeborne.selenide.Selenide.closeWebDriver;
-
-public class UI_TestBase {
-    private static final WebDriverConfig config = ConfigFactory.create(WebDriverConfig.class, System.getProperties());
-
-    @BeforeAll
-    public static void setUp() {
-        RestAssured.baseURI = "https://demoqa.com";
-        RestAssured.defaultParser = Parser.JSON;
-
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.pageLoadStrategy = config.getPageLoadStrategy();
-        Configuration.browserSize = config.getBrowserSize();
-        Configuration.browser = config.getBrowserName().browserToLowerCase();
-        Configuration.browserVersion = config.getBrowserVersion();
-
-        String remoteUrl = config.getRemoteUrl();
-
-        String rwhost = System.getProperty("rwhost");
-        System.out.println("DEBUG: remoteUrl from config = " + remoteUrl);
-        System.out.println("DEBUG: rwhost from system properties = " + rwhost);
-
-        if (remoteUrl != null && !remoteUrl.isEmpty()) {
-            Configuration.remote = remoteUrl;
-        }
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        if (remoteUrl != null && !remoteUrl.isEmpty()) {
-            capabilities.setCapability("selenoid:options", Map.of(
-                    "enableVNC", true,
-                    "enableVideo", true
-            ));
-        } else {
-            if (Objects.equals(Configuration.browser, "chrome")) {
-                WebDriverManager.chromedriver().setup();
-            } else if (Objects.equals(Configuration.browser, "firefox")) {
-                WebDriverManager.firefoxdriver().setup();
-            } else if (Objects.equals(Configuration.browser, "edge")) {
-                WebDriverManager.edgedriver().setup();
-            }
-        }
-        Configuration.browserCapabilities = capabilities;
-    }
-
-
-    @BeforeEach
-    void addListener() {
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-    }
-
-    @AfterEach
-    void addAttachments() {
-        Attach.screenshotAs("Screenshot");
-        if (!Configuration.browser.equals("firefox")) {
-            Attach.pageSource();
-            Attach.browserConsoleLogs();
-        }
-        Attach.addVideo();
-        closeWebDriver();
-    }
-}
-
- */
 package tests.ui_tests;
 
 import com.codeborne.selenide.Configuration;
@@ -126,6 +43,9 @@ public class UI_TestBase {
         log.info("DEBUG: System.getProperty(\"remoteUrl\") = {}", System.getProperty("remoteUrl"));
         log.info("DEBUG: rwhost from system properties = {}", rwhost);
         log.info("DEBUG: remoteUrl из ConfigFactory = '{}'", config.getRemoteUrl());
+        log.info("DEBUG: remoteUrl из ConfigFactory = {}", config.getRemoteUrl());
+        log.info("DEBUG: System.getProperty(\"env\") = {}", System.getProperty("env"));
+
 
         if (remoteUrl != null && !remoteUrl.isEmpty()) {
             log.info("INFO: Тесты запустятся на удаленном сервере: {}", remoteUrl);
