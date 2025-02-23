d9088bfeea32e352268581ca1115410909c44ec7
Evgeniya Shiyanova <evgenshiy@gmail.com>
2025-02-23 23:07:24 +0500

    fixed CredentialsConfig

--- src/test/java/config/CredentialsConfig.java
+++ src/test/java/config/CredentialsConfig.java
@@ -3,6 +3,7 @@ package config;
 import org.aeonbits.owner.Config;
 
 @Config.Sources({
+        "system:properties", // Читаем из -DprofileUserName и -DprofileUserPassword
         "classpath:properties/credentials.properties"
 })
 public interface CredentialsConfig extends Config {
